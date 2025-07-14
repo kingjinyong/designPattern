8b005a305726dff49f17cb0e3d194efba7f08c2f
kingjinyong <wlsdydwhswkf@gmail.com>
2025-07-14 16:51:30 +0900

    어댑터패턴

---
+++ src/adapter/after/AccountUserDetailsService.java
@@ -0,0 +1,17 @@
+package adapter.after;
+import adapter.after.security.UserDetails;
+import adapter.after.security.UserDetailsService;
+
+public class AccountUserDetailsService implements UserDetailsService {
+
+    private AccountService accountService;
+
+    public AccountUserDetailsService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public UserDetails loadUser(String username) {
+        return new AccountUserDetails(accountService.findAccountByUsername(username));
+    }
+}
---
+++ src/adapter/after/App.java
@@ -0,0 +1,15 @@
+package adapter.after;
+
+import adapter.after.security.LoginHandler;
+import adapter.after.security.UserDetailsService;
+
+public class App {
+
+    public static void main(String[] args) {
+        AccountService accountService = new AccountService();
+        UserDetailsService userDetailsService = new AccountUserDetailsService(accountService);
+        LoginHandler loginHandler = new LoginHandler(userDetailsService);
+        String login = loginHandler.login("keesun", "keesun");
+        System.out.println(login);
+    }
+}
