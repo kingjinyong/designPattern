cf6f78346c4de605dc1a9f15f1d35bfef7d6412d
kingjinyong <wlsdydwhswkf@gmail.com>
2025-07-21 16:45:28 +0900

    브릿지패턴

---
+++ src/bridge/before/Champion.java
@@ -0,0 +1,18 @@
+package bridge.before;
+
+
+import bridge.after.Skin;
+
+public interface Champion extends Skin {
+
+    void move();
+
+    void skillQ();
+
+    void skillW();
+
+    void skillE();
+
+    void skillR();
+
+}
