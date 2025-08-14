21e18634f308207afeb68d879ef926ce6db21555
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-14 21:43:38 +0900

    메멘토 패턴

---
+++ src/memento/after/Game.java
@@ -0,0 +1,32 @@
+package memento.after;
+
+public class Game {
+    private int redTeamScore;
+    private int blueTeamScore;
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public GameSave save() {
+        return new GameSave(this.blueTeamScore, this.redTeamScore);
+    }
+
+    public void restore(GameSave gameSave) {
+        this.blueTeamScore = gameSave.getBlueTeamScore();
+        this.redTeamScore = gameSave.getRedTeamScore();
+    }
+
+}
