d8c583a1adf86415d9671a8cbdfe2cf5e6d94a74
kingjinyong <wlsdydwhswkf@gmail.com>
2025-08-18 17:03:35 +0900

    옵저버 패턴

--- src/observer/after/Client.java
+++ src/observer/after/Client.java
@@ -11,12 +11,10 @@ public class Client {
         chatServer.register("오징어게임", user2);
 
         chatServer.register("디자인패턴", user1);
+        chatServer.register("디자인패턴", user2);
 
         chatServer.sendMessage(user1, "오징어게임", "아.. 이름이 기억 남");
+
         chatServer.sendMessage(user2, "디자인패턴", "옵저버 패턴으로 만든 채팅");
-
-        chatServer.unregister("디자인패턴", user1);
-
-        chatServer.sendMessage(user2, "디자인패턴", "옵저버 패턴 장, 단점 보는 중");
     }
 }
